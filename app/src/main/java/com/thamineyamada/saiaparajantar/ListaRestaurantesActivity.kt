b12fa98ad8d06e21b2ffd7eda85014bd688de40c
Thamine Yamada <thamine.yamada@monitoratec.com.br>
2020-08-28 22:52:48 +0100

    primeiro commit

---
+++ app/src/main/java/com/thamineyamada/saiaparajantar/ListaRestaurantesActivity.kt
@@ -0,0 +1,12 @@
+package com.thamineyamada.saiaparajantar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaRestaurantesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_restaurantes)
+
+
+    }
+}
